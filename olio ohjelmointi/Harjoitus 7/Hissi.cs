ab6097123ba2fb3868645e6a64f6d7f3a8c614f1
Patrick Tietäväinen <patrick.tietavainen@student.careeria.fi>
2022-01-11 13:35:53 +0200

    harjoitus 7

---
+++ olio ohjelmointi/Harjoitus 7/Hissi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_7
+{
+    class Hissi
+    {
+        private int minKerros;
+        private int maxKerros;
+        private int nykyinenKerros;
+
+        public Hissi(int minKerros, int maxKerros)
+        {
+            this.minKerros = minKerros;
+            this.maxKerros = maxKerros;
+
+        }
+
+        public int Nykyinenkerros
+        {
+            get => nykyinenKerros;
+            set
+            {
+                if (value > maxKerros || value < minKerros)
+                {
+                    Console.WriteLine("virheellinen Kerros");
+                }
+                else
+                {
+                    nykyinenKerros = value;
+                }
+
+            }
+        }
+    }
+}
---
+++ olio ohjelmointi/Harjoitus 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Harjoitus_7
+{
+    class Program
+    {
+        private static string syöte;
+
+        static void Main(string[] args)
+        {
+            Hissi hissi = new Hissi(1, 6);
+            string syöte;
+            while (true)
+            {
+                Console.WriteLine("Olet kerroksessa" + hissi.Nykyinenkerros);
+                Console.WriteLine("Mihin kerrokseen haluat siirtyä?");
+                syöte = Console.ReadLine();
+
+                if (syöte == "poistu")
+                {
+                    Console.WriteLine("poistuit hissistä");
+                    break;
+
+
+
+                }
+                else
+                {
+                    hissi.Nykyinenkerros = int.Parse(syöte);
+                }
+
+            }
+        }
+    }
+}
