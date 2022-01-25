4e233538d50797c80c92662fb29fe9b66a4c949f
Patrick Tietäväinen <patrick.tietavainen@student.careeria.fi>
2022-01-25 09:53:28 +0200

    kiuas tehtävä

---
+++ olio ohjelmointi/harvia kiuas wpf/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace harvia_kiuas_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+
+
+    public partial class MainWindow : Window
+
+    {
+        kiuas kiuas = new kiuas ();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            kiuas.VaihdaLämpötila(10);
+            kiuas.VaihdaLämpötila(10);
+            kiuas.OnkoPäällä = false;
+        }
+
+
+        private void btn_Lämpötilaup_Click(object sender, RoutedEventArgs e)
+        {
+            int uusiLämpötila = kiuas.Lämpötila + 5;
+            kiuas.VaihdaLämpötila(uusiLämpötila);
+            tb_Lämpötila.Text = kiuas.Lämpötila.ToString();
+        }
+
+        private void btn_Lämpötiladown_Click(object sender, RoutedEventArgs e)
+        {
+            int uusiLämpötila = kiuas.Lämpötila - 5;
+            kiuas.VaihdaLämpötila(uusiLämpötila);
+            tb_Lämpötila.Text = kiuas.Lämpötila.ToString();
+        }
+    }
+}
---
+++ olio ohjelmointi/harvia kiuas wpf/kiuas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harvia_kiuas_wpf
+{
+    class kiuas
+    {
+        public int Lämpötila{ get; set; }
+
+        public int Kosteus { get; set; }
+        
+        public bool OnkoPäällä { get; set; }
+
+        public void VaihdaLämpötila(int uusiLämpötila)
+
+        {
+            Lämpötila = uusiLämpötila;
+
+            if (Lämpötila > 120)
+                Lämpötila = 120;
+            if (Lämpötila < 10)
+                Lämpötila = 10;
+        }
+    
+        public void VaihdaKosteutta(int uusiKosteus)
+
+        {
+            Kosteus = uusiKosteus;
+
+            if (Kosteus > 10)
+                Kosteus = 10;
+            if (Kosteus < 20)
+                Kosteus = 20;
+        }
+    
+    
+    
+    }
+}
